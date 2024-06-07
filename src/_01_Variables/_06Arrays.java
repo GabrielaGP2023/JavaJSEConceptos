e4aeb0d7723d4462420e5487a12bbef1f9561cae
GabrielaGP2023 <cursos@gabrielagomezpuente.es>
2024-06-07 14:53:22 +0200

    Final de Clase 7 Junio - bucles

---
+++ src/_01_Variables/_06Arrays.java
@@ -0,0 +1,30 @@
+package _01_Variables;
+
+import java.util.Scanner;
+
+public class _06Arrays {
+    public static void main(String[] args) {
+        //Longitud de los arrays siempre la misma
+        //Los arrays siempre deben tener el mismo tipo en todas sus celdas
+        int[] numeros = new int[3];
+        int[] numeros2 = {8,9,10}; //Para inicializar los datos del array
+//        numeros[0]= 8;
+//        numeros[1]= 9;
+//        numeros[2]= 10;
+        Scanner scanner = new Scanner(System.in);
+//        for(int i=0; i<4;i++){
+//            System.out.println("Escriba la nota del trimestre " + i );
+//            numeros[i]=scanner.nextInt();
+//        }
+        for(int i=0; i<numeros2.length;i++){
+            System.out.println("Nota del trimestre " + (i+1) + "->" + numeros2[i]);
+        }
+        //Obtener el número más pequeño del array
+
+        scanner.close();
+
+
+
+
+    }
+}
--- src/_05EstructurasControl/_03Bucles.java
+++ src/_05EstructurasControl/_03Bucles.java
@@ -5,8 +5,25 @@ public class _03Bucles {
         for(int i = 1; i <=10; i++){
             System.out.println("Hola mundo " + i);
         }
+        for(int i = 10; i >=1; i--){
+            System.out.println( i);
+        }
         for(int i = 10; i >=0; i--){
             System.out.println(i);
         }
+        //Ejemplo while
+        System.out.println("---While ---");
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+        i=1;
+        //Ejemplo Do while
+        System.out.println("--Do while--");
+        do{
+            System.out.println(i++);
+        }while(i<=10);
     }
 }
